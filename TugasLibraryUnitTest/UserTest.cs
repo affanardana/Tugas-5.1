c282327192495db6b96a9bbb0915466cacc0075a
affanardana <ardanaaffan@students.amikom.ac.id>
2021-10-28 10:53:58 +0700

    first commit

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class UserTest
+    {
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.AreEqual(true, user.IsValidUser("admin", "admin"));
+            Assert.AreEqual(false, user.IsValidUser("Admin", "12345"));
+        }
+    }
+}
